0975a71ce3143d68a68b599233a7ab5ab7c43be3
Yadiel <ycabr035@fiu.edu>
2018-06-15 22:06:45 -0400

    added function

--- Day11/scripts.js
+++ Day11/scripts.js
@@ -24,11 +24,21 @@ function skip() {
 }
 
 function handleRangeUpdate(){
+  video[this.name] = this.value;
 
+}
+
+function handleProgress() {
+  const percent = (video.currentTime/video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
-toggle.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
