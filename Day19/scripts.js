9a5d2025d4fdaa37b4b95e23b1770b973bcf998d
Yadiel <ycabr035@fiu.edu>
2018-06-23 13:54:19 -0400

    added scripts

--- Day19/scripts.js
+++ Day19/scripts.js
@@ -9,9 +9,23 @@ function getVideo() {
   .then (localMediaStream => {
      console.log(localMediaStream);
      video.src = window.URL.createObjectURL(localMediaStream);
-     video.play();  
+     video.play();
 
+  })
+  .catch(err =>{
+    console.error(`OH NO!!!`,err);
   });
+}
+
+function paintToCanvas(){
+const width = video.videoWidth;
+const height = video.videoHeight;
+canvas.width = width;
+canvas.height = height;
+
+setInterval(()=> {
+    ctx.drawImage(video,0,0, width, height);
+})
 }
 
 getVideo();
