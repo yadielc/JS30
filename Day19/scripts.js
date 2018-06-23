9b64a7e80e9ad2e5b4d85c0630b28500215cf50e
Yadiel <ycabr035@fiu.edu>
2018-06-23 13:57:01 -0400

    added scripts

--- Day19/scripts.js
+++ Day19/scripts.js
@@ -5,27 +5,37 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
-  navigator.getUserMedia({video: true, audio:false}) // returns a promise
-  .then (localMediaStream => {
-     console.log(localMediaStream);
-     video.src = window.URL.createObjectURL(localMediaStream);
-     video.play();
-
-  })
-  .catch(err =>{
-    console.error(`OH NO!!!`,err);
-  });
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
 }
 
-function paintToCanvas(){
-const width = video.videoWidth;
-const height = video.videoHeight;
-canvas.width = width;
-canvas.height = height;
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
-setInterval(()=> {
-    ctx.drawImage(video,0,0, width, height);
-})
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
 }
 
 getVideo();
