05905e1e4cf54aae7755d85cb739356812a03d79
Yadiel <ycabr035@fiu.edu>
2018-06-23 14:31:44 -0400

    added greenScreen function

--- Day19/scripts.js
+++ Day19/scripts.js
@@ -33,7 +33,7 @@ function paintToCanvas() {
     // pixels = redEffect(pixels);
 
     pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.1; 
+    ctx.globalAlpha = 0.1;
     // ctx.globalAlpha = 0.8;
 
     // pixels = greenScreen(pixels);
@@ -78,6 +78,34 @@ function rgbSplit(pixels){
   }
   return pixels;
 }
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
 
 getVideo();
 
