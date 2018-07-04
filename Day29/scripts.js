e578f4536b0d5c43c8caf27cb2cb260d867476e1
Yadiel <ycabr035@fiu.edu>
2018-07-04 12:38:30 -0400

    added function

--- Day29/scripts.js
+++ Day29/scripts.js
@@ -38,6 +38,7 @@ function displayTimeLeft(seconds){
 function displayEndTime(timestamp) {
    const end = new Date(timestamp);
    const hour = end.getHours();
+   const adjustedHour = hour > 12 ? hour - 12 : hour; 
    const minutes = end.getMinutes();
    endTime.textContent = `Be Back At ${hour}:${minutes}`;
 
