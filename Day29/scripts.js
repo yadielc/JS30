30f020f19ce7b3794d0193f13d49e2c9b8fd7cee
Yadiel <ycabr035@fiu.edu>
2018-07-04 12:31:05 -0400

    added function

--- Day29/scripts.js
+++ Day29/scripts.js
@@ -29,8 +29,7 @@ function displayTimeLeft(seconds){
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
-  document.title = display; 
+  document.title = display;
   timerDisplay.textContent  = display;
-
 
 }
