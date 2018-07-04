c0a43dd9902d740ce500cb448ed8c934ee2d71a9
Yadiel <ycabr035@fiu.edu>
2018-07-04 12:22:07 -0400

    added function

--- Day29/scripts.js
+++ Day29/scripts.js
@@ -3,7 +3,13 @@ function timer(seconds) {
 
    const now = Date.now();
    const then = now + seconds * 1000;
-   setInterval();
+   setInterval(() => {
+
+     const secondsLeft = (then - Date.now())/ 1000;
+
+   }, 1000); 
+
+ );
 
 
 }
