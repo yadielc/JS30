7769977af1543b1d17160d88b4661485fabbef96
Yadiel <ycabr035@fiu.edu>
2018-07-04 12:24:42 -0400

    added function

--- Day29/scripts.js
+++ Day29/scripts.js
@@ -3,7 +3,9 @@ function timer(seconds) {
 
    const now = Date.now();
    const then = now + seconds * 1000;
-   setInterval(() => {
+   displayTimeLeft(seconds);
+
+  countdown =  setInterval(() => {
 
      const secondsLeft = Math.round((then - Date.now())/ 1000);
      if(secondsLeft < 0){
