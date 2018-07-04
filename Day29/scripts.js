f735f6a71f2096a21de48df3aa2a924a3e03acf2
Yadiel <ycabr035@fiu.edu>
2018-07-04 12:41:00 -0400

    added function

--- Day29/scripts.js
+++ Day29/scripts.js
@@ -1,7 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('display__end-time');
-
+const buttons = document.querySelectorAll('[data-time]');
 function timer(seconds) {
 
 
